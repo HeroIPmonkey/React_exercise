46cf21b0f704a20422af3081231596afe612575f
monkey1992719 <mykhailovirii@gmail.com>
2021-03-04 17:31:52 +0900

    How to Handle Routing in a React Application using the React Router library

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { BrowserRouter as Router } from "react-router-dom"
+
 //component file
 import TodoContainer from './functionBased/components/TodoContainer'
 //stylesheet
 import "./functionBased/App.css"
+
+
 ReactDom.render(
   <React.StrictMode>
-    <TodoContainer />
-  </React.StrictMode>
-  ,document.getElementById('root'))
+    <Router>
+      <TodoContainer />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById("root")
+)
